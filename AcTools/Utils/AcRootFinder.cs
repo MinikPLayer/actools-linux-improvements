aff1ce0422763de07911c306c2eeaa5cd67cce13
Michał Tomecki <minexplaypl@gmail.com>
2026-05-15 02:20:09 +0100

    Improved Linux AC install directory detection

--- AcTools/Utils/AcRootFinder.cs
+++ AcTools/Utils/AcRootFinder.cs
@@ -8,7 +8,72 @@ using Microsoft.Win32;
 
 namespace AcTools.Utils {
     public static class AcRootFinder {
+        private static string GetLinuxSteamAppsDirectory()
+        {
+            const string BASE_PATH = "Z:\\home";
+            const string SUB_PATH = ".steam\\steam\\steamapps";
+
+            AcToolsLogging.Write("Searching for a linux path...");
+
+            var user = Environment.GetEnvironmentVariable("USER");
+            if (string.IsNullOrEmpty(user))
+            {
+                AcToolsLogging.Write("User env variable not defined.");
+                return null;
+            }
+
+            var userHome = Path.Combine(BASE_PATH, user);
+            if (!Directory.Exists(Path.Combine(userHome)))
+            {
+                AcToolsLogging.Write("User home doesn't exist.");
+                return null;
+            }
+
+            try
+            {
+                var homeUserAcPath = Path.Combine(userHome, SUB_PATH);
+                if (Directory.Exists(homeUserAcPath))
+                {
+                    return homeUserAcPath;
+                }
+
+                AcToolsLogging.Write("Cannot find Linux assetto corsa directory.");
+                return null;
+            }
+            catch (Exception e)
+            {
+                AcToolsLogging.Write(e);
+            }
+
+            return null;
+        }
+
+        private static string GetSteamAppsDirectoryFromCurrentPath()
+        {
+            var cwd = Directory.GetCurrentDirectory();
+            var target = Path.Combine(cwd, "..\\..\\");
+
+            if (Directory.Exists(target))
+                return target;
+            
+            return null;
+        }
+
         private static IEnumerable<string> GetSteamAppsDirectories() {
+            var linuxSteamAppsDirectory = GetLinuxSteamAppsDirectory();
+            if (linuxSteamAppsDirectory != null)
+            {
+                AcToolsLogging.Write($"Got Steam apps directory (linux): ${linuxSteamAppsDirectory}");
+                yield return linuxSteamAppsDirectory;
+            }
+
+            var steamAppsCwdDir = GetSteamAppsDirectoryFromCurrentPath();
+            if (steamAppsCwdDir != null)
+            {
+                AcToolsLogging.Write($"Got Steam apps directory from CWD: ${linuxSteamAppsDirectory}");
+                yield return linuxSteamAppsDirectory;
+            }
+
             var regKey = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam");
             if (regKey == null) yield break;
 
@@ -27,55 +92,8 @@ namespace AcTools.Utils {
             }
         }
 
-        private static string TryToFindLinux()
-        {
-            const string BASE_PATH = "Z:\\home";
-            const string SUB_PATH = ".steam\\steam\\steamapps\\common\\assettocorsa";
-
-            AcToolsLogging.Write("Searching for a linux path...");
-
-            var user = Environment.GetEnvironmentVariable("USER");
-            if (string.IsNullOrEmpty(user))
-            {
-                AcToolsLogging.Write("User env variable not defined.");
-                return null;
-            }
-
-            var userHome = Path.Combine(BASE_PATH, user);
-            if(!Directory.Exists(Path.Combine(userHome)))
-            {
-                AcToolsLogging.Write("User home doesn't exist.");
-                return null;
-            }
-
-            try
-            {
-                var homeUserAcPath = Path.Combine(userHome, SUB_PATH);
-                if(Directory.Exists(homeUserAcPath))
-                {
-                    return homeUserAcPath;
-                }
-
-                AcToolsLogging.Write("Cannot find Linux assetto corsa directory.");
-                return null;
-            }
-            catch (Exception e)
-            {
-                AcToolsLogging.Write(e);
-            }
-
-            return null;
-        }
-
         [CanBeNull]
         public static string TryToFind() {
-            if (UnixUtils.IsUnixWine)
-            {
-                var linuxPath = TryToFindLinux();
-                if (linuxPath != null)
-                    return linuxPath;
-            }
-
             try {
                 var regKey = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam");
                 return regKey == null ? null :
